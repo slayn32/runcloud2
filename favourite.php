67b45b2f330ddde6a5fef09647ca8678112ddbc7
slayn32 <56430991+slayn32@users.noreply.github.com>
2019-10-11 17:07:44 +0800

    Add files via upload

---
+++ favourite.php
@@ -0,0 +1,17 @@
+<?php
+$favcolor = "red";
+
+    switch($favcolor){
+        case "red";
+             echo "your favourite color is red!";
+             break;
+		case "blue";
+             echo "your favourite color is blue!";
+             break;
+        case "green";
+             echo "your favourite color is green!";
+             break;
+        default:
+             echo "you don't have favourite color.";
+    }
+?>
